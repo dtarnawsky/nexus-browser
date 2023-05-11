d2e8f792e5895dac7268530566321ce49f8ea0c2
dtarnawsky <damian@ionic.io>
2023-05-11 14:56:45 -0700

    fix: missing routerModule

--- src/app/privacy/privacy.page.ts
+++ src/app/privacy/privacy.page.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
